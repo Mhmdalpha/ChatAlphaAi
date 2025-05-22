51a50d5acc53438ab3b0bbc16607ecbfb18cab45
alpha <muhammadalphaseptian@gmail.com>
2025-05-22 15:44:46 +0700

    first commit

---
+++ client/src/routes/signInPage/signInPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SignIn } from "@clerk/clerk-react";  // Import komponen SignIn dari Clerk
+import "./signInPage.css";  // Mengimpor CSS untuk halaman sign in
+
+const SignInPage = () => {
+  return (
+    <div className="signInPage">
+      <SignIn
+        path="/sign-in"  // Path untuk halaman sign in
+        signUpUrl="/sign-up"  // URL untuk halaman sign up
+        forceRedirectUrl="/dashboard"  // Setelah berhasil sign in, redirect ke halaman dashboard
+      />
+    </div>
+  );
+};
+
+export default SignInPage;  // Ekspor komponen SignInPage
